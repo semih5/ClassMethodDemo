cd4d16608fe8194dd48a6b47552ecbac00a14cb1
Muhammed Semih Kundakcı <semihkundakci5@gmail.com>
2021-01-21 16:29:34 +0300

    completed

--- ClassMethodDemo/Customer.cs
+++ ClassMethodDemo/Customer.cs
@@ -6,7 +6,7 @@ namespace ClassMethodDemo
 {
     class Customer
     {
-        public int Id { get; set; }
+        public int Id { get; set; } 
         public string CustomerName { get; set; }
         public double Balance { get; set; }
        
